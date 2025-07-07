27481848cb95fa4637f3aa25d0e80e3b272e4def
abdcasawi <abdcasawi2023@gmail.com>
2025-07-07 12:51:26 +0200

    Updated LiveTV.tsx

--- src/components/LiveTV.tsx
+++ src/components/LiveTV.tsx
@@ -392,11 +392,10 @@ const LiveTV: React.FC<LiveTVProps> = ({ onChannelSelect }) => {
     },
     {
       "id": "97",
-      "name": "Rai 1",
+      "name": "Mytime Movies",
       "logo": "https://cdn.jsdelivr.net/gh/Tundrak/IPTV-Italia/logos/rai1.png",
 "streamUrl":
-  "https://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/608aa8a5709d6b0007b132fe/master.m3u8?advertisingId=%7BPSID%7D&appVersion=unknown&deviceDNT=%7BTARGETOPT%7D&deviceId=%7BPSID%7D&deviceLat=0&deviceLon=0&deviceMake=samsung&deviceModel=samsung&deviceType=samsung-tvplus&deviceVersion=unknown&embedPartner=samsung-tvplus&profileFloor=&profileLimit=&samsung_app_domain=%7BAPP_DOMAIN%7D&samsung_app_name=%7BAPP_NAME%7D&us_privacy=1YNY",
-      "category": "Rai"
+  "https://mytimefrance-rakuten-samsung.amagi.tv/playlist.m3u8": "Rai"
     },
     {
       "id": "98",
